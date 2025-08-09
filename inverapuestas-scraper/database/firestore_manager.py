d0ad156d6402f164482370304b3ebceab6174cc9
fabian mls <fabiantonio.mls@gmail.com>
2025-08-09 17:39:07 +0000

    Esquema Propuesto para Inverapuestas Pro Se propone una estructura de da

--- inverapuestas-scraper/database/firestore_manager.py
+++ inverapuestas-scraper/database/firestore_manager.py
@@ -1,4 +1,3 @@
 # Este módulo gestionará todas las interacciones con la base de datos Firestore.
 # Se encargará de conectar con la base de datos, borrar los datos antiguos
 # y escribir los nuevos datos extraídos por el scraper de forma atómica.
-
